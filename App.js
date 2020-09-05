b96fa20f76b642dc22ed280c756c78ea6dc0877a
umairsadiq88 <umairsadiq2010@gmail.com>
2020-09-05 13:33:21 +0500

    task 1

---
+++ App.js
@@ -0,0 +1,11 @@
+/**
+|--------------------------------------------------
+| TASK 1
+|--------------------------------------------------
+Create A program that takes a name from the user and append "Mr." at the start of the name.**/
+
+
+let name = prompt ("Please Enter your name", )
+
+alert ("Mr" + name)
+
