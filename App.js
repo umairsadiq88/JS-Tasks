8229240eafdd0c6e79ae2dacdf2281137903bb90
umairsadiq88 <umairsadiq2010@gmail.com>
2020-09-10 18:11:53 +0500

    update App.js

--- App.js
+++ App.js
@@ -4,8 +4,93 @@
 |--------------------------------------------------
 Create A program that takes a name from the user and append "Mr." at the start of the name.**/
 
+// let name = prompt ("Please Enter your name", )
+// alert ("Mr." + name);
 
-let name = prompt ("Please Enter your name", )
+//Task 1 completed
 
-alert ("Mr" + name)
+
+// TASK 2
+
+// Create a program that takes an input from the user as a string, count the number of characters and show it in alert
+// HINT =====> String is an array of characters in javascript's core
+
+// let name = prompt ("Please Enter your name", )
+// var nameCount = name.length;
+
+// alert ("Your Characters in your Name is" + " " + nameCount);
+
+//Task 2 completed
+
+
+// Task 3
+
+// Create a program that takes an input from user as a string and remove any whitespaces in the input given.
+// HINT ====> search for "trim" in javascript
+
+
+// let quote = prompt ("Please Enter your favourite quote", )
+// var trimQuote = quote.trim();
+
+// alert (trimQuote);
+
+//Task 3 complete
+
+// TASK 4
+
+// Which of the following variable names are incorrect? 
+// 1. _name
+// 2. $name_1
+// 3. 1_#name
+// 4. #name
+
+// Corrected variable
+// 1. _name
+// 2. $name_1
+
+// //Task 4 complete
+
+
+// TASK 5
+
+// Solve the equation below and the answer should not be in float point value
+
+// 2 + 6 * 8 - 9 + (2/3) + 9
+
+// var equv = (2 + 6 * 8 - 9 + (2/3) + 9);
+// var answer = Math.round(equv);
+// alert (answer);
+
+// //Task 5 complete
+
+
+// TASK 6
+
+// What will be the output of the following program? 
+
+// var product1 = 200 * 3;
+// var product2 = 300; 
+// var discount = 20; 
+// var total = (product1 + product2) / discount;
+
+// alert(total);
+
+// //Task 6 complete
+
+// TASK 7
+// write a program that performs the following operations
+
+
+1. data type of phone number should be a number 
+2. data type of NIC should be string 
+3. add a greeting before the name e.g hello mr.{name}
+
+
+data [
+phoneNumber = 03205757562 
+var NIC = 4250187968352
+var name = "umair"
+
+]
+
 
