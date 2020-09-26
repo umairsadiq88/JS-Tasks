efda0f296c3966f3fa006ff58452bbca406afccb
umairsadiq88 <umairsadiq2010@gmail.com>
2020-09-26 17:34:09 +0500

    update

--- Tasks2.js
+++ Tasks2.js
@@ -6,19 +6,32 @@
 
 
 
-let myObj = {
+// let myObj = {
 
-    name: "Umair Sadiq",
-    position: "Assist. Manager Marketing",
-    company: "SIND MEDICAL Stores",
-    address: {
-        address: 1
-    }
-}
+//     name: "Umair Sadiq",
+//     position: "Assist. Manager Marketing",
+//     company: "SIND MEDICAL Stores",
+//     address: {
+//         address: 1
+//     }
+// }
 
-let objectKeys = Object.keys(myObj);
-// console.log(objectKeys);
+// let objectKeys = Object.keys(myObj);
+// // console.log(objectKeys);
 
-for (let i= 0; i < objectKeys.length; i++) {
-    console.log(objectKeys[i]);
-}
+// for (let i= 0; i < objectKeys.length; i++) {
+//     console.log(objectKeys[i]);
+// }
+
+// 19 Sep 2020
+// Chapters
+
+// 1) try & catch
+
+// 2) for of Loop 
+
+// 3) for in Loop 
+
+// 4) MAP
+
+// 5) filter
